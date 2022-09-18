efb239aa6627a424aeec14a5754c52f9806b2b14
elainexuacc <can.xu@g.austincc.edu>
2022-09-18 01:56:08 +0000

    hw 2 with test cases

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,15 +1,33 @@
 ﻿//write include statements
-
+#include "hwexpression.h"
+#include<iostream>
 
 //write namespace using statement for cout
-
+using std::cout;
+using std::cin;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
+	double meal_amount;
+	cin >> meal_amount;
 	
+	double tax_amount;
+	tax_amount = get_sales_tax_amount(meal_amount);
 
-	return 0;
+	double tip_rate;
+	cin >> tip_rate;
+
+	double tip_amount;
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+	
+	double total;
+	total = meal_amount + tax_amount + tip_amount;
+	
+	cout<<"Meal Amount: "<<meal_amount<<"\n";
+	cout<<"Sales Tax: "<<tax_amount<<"\n";
+	cout<<"Tip Amount: "<<tip_amount<<"\n";
+	cout<<"Total: "<<total<<"\n";
 }

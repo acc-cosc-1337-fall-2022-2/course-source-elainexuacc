2a62c3bc2bab1a630eb85d14885315871f7a042c
elainexuacc <can.xu@g.austincc.edu>
2022-09-24 03:22:41 +0000

    HW 3 with test cases

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,45 @@
 //write include statement for decisions header
-
+#include "decisions.h"
 
 //Write code for function(s) code here
+char get_letter_grade_using_if(int grade)
+{
+    if(grade >= 90 && grade <= 100)
+        return 'A';
+    else if(grade >= 80 && grade <= 89)
+        return 'B';
+    else if(grade >= 70 && grade <= 79)
+        return 'C';
+    else if(grade >= 60 && grade <= 69)
+        return 'D';
+    else
+        return 'F';
+}
+
+char get_letter_grade_using_switch(int grade)
+{
+    int x=grade/10;
+    char letter_grade;
+    switch(x)
+    {
+        case 10:
+            letter_grade='A';
+            break;
+        case 9:
+            letter_grade='A';
+            break;
+        case 8:
+            letter_grade='B';
+            break;
+        case 7:
+            letter_grade='C';
+            break;
+        case 6:
+            letter_grade='D';
+            break;
+        default:
+            letter_grade='F';
+            break;
+    }
+    return letter_grade;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,31 @@
 //write include statements
+#include <iostream>
+#include "decisions.h"
+
+using std::cout;
+using std::cin;
+using std::string;
+
 
 int main() 
 {
-	return 0;
+	int grade;
+	cin >> grade;
+
+	if(grade >= 0 && grade <= 100)
+	{
+		string letter_grade_if;
+		letter_grade_if = get_letter_grade_using_if(grade);
+		cout<<"Letter grade using if: "<<letter_grade_if<<"\n";
+
+		string letter_grade_switch;
+		letter_grade_switch = get_letter_grade_using_switch(grade);
+		cout<<"Letter grade using switch: "<<letter_grade_switch<<"\n";
+	}
+	else
+	{
+		cout<<"The number you entered is out of range."<<"\n";
+	}
+		
+
 }
